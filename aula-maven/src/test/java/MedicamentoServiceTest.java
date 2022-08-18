e9c47d74226742885f12ff9e6dc94e3550f33e2b
tatianegardin <contatotatianegardin@hotmail.com>
2022-08-18 20:33:38 -0300

    aula maven

---
+++ aula-maven/src/test/java/MedicamentoServiceTest.java
@@ -0,0 +1,18 @@
+import medicamentos.dao.ConfiguracaoJDBC;
+import medicamentos.dao.impl.MedicamentoDaoImpl;
+import medicamentos.model.Medicamento;
+import medicamentos.service.MedicamentoService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MedicamentoServiceTest {
+    private MedicamentoService medicamentoService = new MedicamentoService(new MedicamentoDaoImpl(new ConfiguracaoJDBC()));
+
+    @Test
+    public void salvarMedicamento() {
+        Medicamento medicamento = new Medicamento("Ibuprofeno", "Aché", 395, 23.0);
+        medicamentoService.salvar(medicamento);
+        assertTrue(medicamento.getId() != null);
+    }
+}
